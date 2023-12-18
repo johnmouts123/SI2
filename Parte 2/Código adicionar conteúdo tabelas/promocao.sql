9333891661c6a5ef98bc5999da2e6927dc51bfe1
João Mota <joaochoupinamota@hotmail.com>
2023-12-18 15:47:29 +0000

    .

---
+++ Parte 2/Código adicionar conteúdo tabelas/promocao.sql
@@ -0,0 +1,3 @@
+insert into promocao (id_promocao, tipo_promo, descricao_promo, duracao_promo) values (1, 'reduções temporárias de preços', 10, 47);
+insert into promocao (id_promocao, tipo_promo, descricao_promo, duracao_promo) values (2, 'cupões em jornais', 20, 25);
+insert into promocao (id_promocao, tipo_promo, descricao_promo, duracao_promo) values (3, 'cartazes na loja', 30, 1);
